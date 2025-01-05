600102de01aa7f7942a5fbe23338bd2fce306394
SahilMern <sahil9082811@gmail.com>
2025-01-05 20:18:56 +0530

    adding tricky question in react js

--- test.js
+++ test.js
@@ -1,66 +1,66 @@
-// function abc() {
-//   console.log(abc.xyz);
-// }
-// abc();
-// abc.xyz = 400;
-// abc.xyz = 200;
-// abc();
+// // function abc() {
+// //   console.log(abc.xyz);
+// // }
+// // abc();
+// // abc.xyz = 400;
+// // abc.xyz = 200;
+// // abc();
 
-// const number = [44, 85, 98, 5];
-// number[100] = 100;
-// console.log(number); //Output:- 44, 85, 98, 5, <96 empty items>, 100 ]
-// console.log(number[20]); // Undefined
+// // const number = [44, 85, 98, 5];
+// // number[100] = 100;
+// // console.log(number); //Output:- 44, 85, 98, 5, <96 empty items>, 100 ]
+// // console.log(number[20]); // Undefined
 
-// console.log(typeof typeof 144); //string
-// const array = [..."sahil"];
-// console.log(array, "arrray");
-// ["s", "a", "h"];
+// // console.log(typeof typeof 144); //string
+// // const array = [..."sahil"];
+// // console.log(array, "arrray");
+// // ["s", "a", "h"];
 
-// //!NOTE parseInt function string ko left se right tak padhta hai aur jab tak valid number milta hai, tab tak usse parse karta hai.
-// console.log(parseInt("10+2ss")); //10
-// console.log(parseInt("7FM")); //7
-// console.log(parseInt("M7F")); //Nan
+// // //!NOTE parseInt function string ko left se right tak padhta hai aur jab tak valid number milta hai, tab tak usse parse karta hai.
+// // console.log(parseInt("10+2ss")); //10
+// // console.log(parseInt("7FM")); //7
+// // console.log(parseInt("M7F")); //Nan
 
-// console.log(isNaN("Praveen"));
-// console.log(
-//   [1, 2].map((num) => {
-//     if (num > 0) return "s";
-//     return num * 2;
-//   })
-// );
+// // console.log(isNaN("Praveen"));
+// // console.log(
+// //   [1, 2].map((num) => {
+// //     if (num > 0) return "s";
+// //     return num * 2;
+// //   })
+// // );
 
-// const obj = {
-//     a: 10,
-//     a: 20,
-//     a: 30
-//   };
-//   console.log(obj); //output:30
+// // const obj = {
+// //     a: 10,
+// //     a: 20,
+// //     a: 30
+// //   };
+// //   console.log(obj); //output:30
 
-// const abc1 = () => {
-//   return ;
-// }
-// console.log(abc1());
+// // const abc1 = () => {
+// //   return ;
+// // }
+// // console.log(abc1());
 
-// {
-//   function abc3() {
-//     console.log("hello");
-//   }
-// }
-// abc3(); //abc3 is not defined
+// // {
+// //   function abc3() {
+// //     console.log("hello");
+// //   }
+// // }
+// // abc3(); //abc3 is not defined
 
 
-// let count = 0;
-// const num = [0, 1, 2, 3];
-// num.forEach((elem) => {
-//   if (elem) {
-//     console.log("hey");
-//     count++;
-//   }
-// });
-// console.log(count); 
+// // let count = 0;
+// // const num = [0, 1, 2, 3];
+// // num.forEach((elem) => {
+// //   if (elem) {
+// //     console.log("hey");
+// //     count++;
+// //   }
+// // });
+// // console.log(count); 
 
-const arr = [4,8,5,6,2]
-// arr.splice(2,5)
-arr.unshift(20)
-console.log(arr, "ar"); //remvoe
+// const arr = [4,8,5,6,2]
+// // arr.splice(2,5)
+// arr.unshift(20)
+// console.log(arr, "ar"); //remvoe
 
